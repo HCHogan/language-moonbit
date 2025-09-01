93f9becd61275d89c4f805d5405e11250645ccb5
HCHogan <ysh2291939848@outlook.com>
2025-09-01 20:52:35 +0800

    use haskell.nix

--- flake.nix
+++ flake.nix
@@ -1,57 +1,41 @@
 {
-  description = "My flake template";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = inputs @ {
-    flake-parts,
-    rust-overlay,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    let
+      supportedSystems = [
         "x86_64-linux"
+        "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: let
-        packages = with pkgs; [
-          cmake
-          ninja
-          gnumake
-          haskell.compiler.ghc912
-          haskell.packages.ghc912.haskell-language-server
-          cabal-install
+    in
+      flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        overlays = [ haskellNix.overlay
+          (final: _prev: {
+            hixProject =
+              final.haskell-nix.hix.project {
+                src = ./.;
+                # uncomment with your current system for `nix flake show` to work:
+                evalSystem = "x86_64-linux";
+              };
+          })
         ];
-      in {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config = {};
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            inherit packages;
-          };
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake {};
+      in flake // {
+        legacyPackages = pkgs;
+      });
 
-          clang = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
-            inherit packages;
-          };
-        };
-      };
-    };
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
 }
